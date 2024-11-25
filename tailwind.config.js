428ee33fd2faf7f2741af87d1221d2aaa06d3e09
Simo Virtanen <simo@sakeus.fi>
2024-11-25 11:11:40 +0200

    Slush post

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,md,liquid,erb,serb,rb}',
-    './frontend/javascript/**/*.js',
-  ],
-  theme: {
-  colors: {
-   }, 
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
-
+	content: [
+		"./src/**/*.{html,md,liquid,erb,serb,rb}",
+		"./frontend/javascript/**/*.js",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("@tailwindcss/typography")],
+};
